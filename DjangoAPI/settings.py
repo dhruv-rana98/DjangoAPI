36a5ca34e9b601b2cef1459293862457c8d08ed9
Dhruv Rana <dhruvrana0000@gmail.com>
2022-09-20 20:43:18 +0530

    Connecting to the server successful and migrating the models

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import pymysql
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'LibraryApp.apps.LibraryappConfig'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,11 +79,19 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': '3002',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            "init_command": "SET sql_mode = 'STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- LibraryApp/models.py
+++ LibraryApp/models.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Admin(models.Model):
+    AdminId = models.AutoField(primary_key=True)
+    AdminName = models.CharField(max_length=500)
+    AdminPassword = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+    BookId = models.AutoField(primary_key=True)
+    BookName = models.CharField(max_length=500)
+    BookCategory = models.CharField(max_length=500)
+    BookAuthor = models.CharField(max_length=500)
